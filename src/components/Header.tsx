bac605fdb2782a30fbd66a6d43b273d20831904f
Alessandro Palazzesi <alessandro.palazzesi@gmail.com>
2024-07-30 20:52:17 +0200

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,34 @@
-'use client';
+"use client";
 
 import { Role } from "@/app/types";
 import Link from "next/link";
 import Button from "./Button";
-import { setCookie } from "cookies-next";
+import { getCookie, setCookie } from "cookies-next";
 import { AUTHORIZATION_COOKIE_KEY } from "@/app/api.service";
+import { useEffect } from "react";
+import { decode } from "jsonwebtoken";
 
 type HeaderProps = {
   role: Role;
 };
 
 export const Header = ({ role }: HeaderProps) => {
-
   const handleLogout = () => {
     setCookie(AUTHORIZATION_COOKIE_KEY, "");
-    window.location.href = "/login"
-  }
+    window.location.href = "/login";
+  };
+
+  useEffect(() => {
+    const token = getCookie(AUTHORIZATION_COOKIE_KEY);
+    if (token) {
+      const decodedToken = decode(token) as { exp?: number };
+      const currentTime = Math.floor(Date.now() / 1000);
+      if (!decodedToken.exp || decodedToken.exp <= currentTime) {
+        setCookie(AUTHORIZATION_COOKIE_KEY, "");
+        window.location.href = "/login";
+      }
+    }
+  }, []);
 
   return (
     <div className="h-12 border-b bg-slate-100 w-full flex justify-center items-center gap-12">
@@ -35,7 +48,11 @@ export const Header = ({ role }: HeaderProps) => {
           Search doctor
         </Link>
       ) : null}
-      <Button className=" absolute right-4 max-w-[100px]" label="Logout" onClick={handleLogout} /> 
+      <Button
+        className=" absolute right-4 max-w-[100px]"
+        label="Logout"
+        onClick={handleLogout}
+      />
     </div>
   );
 };
